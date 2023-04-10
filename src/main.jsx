2056b6c5dadff3d0627b77f7fa2bc461af9f8a26
Your Name <perezfernando95@gmail.com>
2023-04-09 22:44:27 -0300

    Adding Redux-Persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/GlobalStyles";
-import { store } from "./redux/store/store";
+import { persistor, store } from "./redux/store/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyle />
-      <App />
+      <PersistGate persistor={persistor}>
+        <GlobalStyle />
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import taskReducer from '../reducers/taskReducers/taskSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+const reducers = combineReducers({
+    task: taskReducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['task'],
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-    reducer: {
-        task: taskReducer,
-    }
+    reducer: persistedReducer
 })
+
+export const persistor = persistStore(store)
